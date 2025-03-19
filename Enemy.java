e1f22c456dfd2988a33773ca5d27ec52273da629
Hypnoticat <spyracat@gmail.com>
2025-03-19 02:59:10 +0000

    Silly Mistake
    ArrayList.Contains cannot detect Arrays but can detect ArrayLists so swapped out the int arrays with array lists

--- Enemy.java
+++ Enemy.java
@@ -7,7 +7,7 @@ public class Enemy {
     private Generator gen;
     private Player player;
     private ArrayList<double[]> listLocations;
-    private ArrayList<double[]> visited;
+    private ArrayList<ArrayList<Integer>> visited;
     private double[] targetLocation;
     private boolean moving;
     private double speed = 0.01;
@@ -40,35 +40,36 @@ public class Enemy {
 
     public int search(int depth, int x, int y){
         System.out.println(x + " " + y);
-        visited.add(new double[] {x,y});
+        visited.add(new ArrayList<Integer>(Arrays.asList(x,y)));
         if(x == (int)xPos && y == (int)yPos){
+            listLocations.removeFirst();
             return 1;
         }
         if(depth > 0){
             if(x == 0 || y == 0 || x == gen.mapSize - 1 || y == gen.mapSize - 1 || (Math.abs((int)xPos - x) + Math.abs((int)yPos - y)) > depth){
                 return 0;
             }
-            if(!visited.contains(new double[] {x + 1, y}) && gen.area[y][x + 1] == 0){
+            if(!(visited.contains(new ArrayList<Integer>(Arrays.asList(x + 1,y)))) && gen.area[y][x + 1] == 0){
                 if(search(depth - 1, x + 1, y) == 1){
                     listLocations.add(new double[]{x + 0.5, y + 0.5});
                     return 1;
                 }
             }
-            if(!visited.contains(new double[] {x - 1, y}) && gen.area[y][x - 1] == 0) {
+            if(!(visited.contains(new ArrayList<Integer>(Arrays.asList(x - 1,y)))) && gen.area[y][x - 1] == 0) {
                 if (search(depth - 1, x - 1, y) == 1) {
                     listLocations.add(new double[]{x + 0.5, y + 0.5});
                     return 1;
                 }
             }
 
-            if(!visited.contains(new double[] {x, y + 1}) && gen.area[y + 1][x] == 0) {
+            if(!(visited.contains(new ArrayList<Integer>(Arrays.asList(x,y + 1)))) && gen.area[y + 1][x] == 0) {
                 if (search(depth - 1, x, y + 1) == 1) {
                     listLocations.add(new double[]{x + 0.5, y + 0.5});
                     return 1;
                 }
             }
 
-            if(!visited.contains(new double[] {x, y - 1}) && gen.area[y - 1][x] == 0) {
+            if(!(visited.contains(new ArrayList<Integer>(Arrays.asList(x,y - 1)))) && gen.area[y - 1][x] == 0) {
                 if (search(depth - 1, x, y - 1) == 1) {
                     listLocations.add(new double[]{x + 0.5, y + 0.5});
                     return 1;
